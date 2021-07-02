5ed42d08141ca475150897dc9a6d24d1c4a0286b
SL <samanthl795@gmail.com>
2021-07-01 22:30:27 -0700

    Show current day's weather

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@
 var searchButtonEl = $("#searchButton");
 var searchInputEl = $("#searchInput");
 var searchedCitiesListEl = $("#searched-cities-list");
+var todaysWeatherEl = $("#todays-weather");
+var API_KEY = "3693a2a576a9f86b3d300687b396e588";
 
 // List of already searched cities
 var searchCitiesList = [];
@@ -32,6 +34,49 @@ function displaySearchedCities() {
     }
 }
 
+// Get the weather for a city
+function getWeatherForCity(city) {
+    // Encode the URI to escape spaces in the city
+    var requestUrl = encodeURI('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + API_KEY + '&units=imperial');
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var todaysDate = moment(data.dt, "X").format("MM-DD-YYYY");
+            var icon = data.weather.icon;
+            var iconDescription = data.weather.description;
+            todaysWeatherEl.empty();
+            var cityAndDateEl = $("<h1>");
+            cityAndDateEl.text(city + " (" + todaysDate + ")");
+            todaysWeatherEl.append(cityAndDateEl);
+            var tempEl = $("<div>");
+            tempEl.text("Temp: " + data.main.temp + "°F");
+            todaysWeatherEl.append(tempEl);
+            var windEl = $("<div>");
+            windEl.text("Wind: " + data.wind.speed + " MPH");
+            todaysWeatherEl.append(windEl);
+            var humidityEl = $("<div>");
+            humidityEl.text("Humidity: " + data.main.humidity + " %");
+            todaysWeatherEl.append(humidityEl);
+            // Show the card
+            // Get the UV index and 5 day forecase
+            requestUrl = encodeURI("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=hourly&appid=" + API_KEY);
+            console.log(requestUrl);
+            return fetch(requestUrl);
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var uviEl = $("<div>");
+            uviEl.text("UV Index: " + data.current.uvi);
+            todaysWeatherEl.append(uviEl);
+            todaysWeatherEl.show();
+        });
+}
+
 // Click handler for the search button
 searchButtonEl.on("click", function() {
     var city = searchInputEl.val();
@@ -52,4 +97,5 @@ searchButtonEl.on("click", function() {
 
     // Clear the input field's value
     searchInputEl.val("");
+    getWeatherForCity(city);
 });
